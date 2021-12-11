d2945c5b8dfc2557cba534ce1b3a27858f6c5a5e
Leoxyzua <apinky549@gmail.com>
2021-12-10 21:58:04 -0500

    🐛 Finding a fix

--- src/interactions/handleCommands.ts
+++ src/interactions/handleCommands.ts
@@ -1,74 +1,76 @@
 import {
-	APIInteractionResponse,
-	APIChatInputApplicationCommandInteraction,
-	ApplicationCommandOptionType,
-	ApplicationCommandInteractionDataOptionString,
-	InteractionResponseType,
-	MessageFlags,
+    APIInteractionResponse,
+    APIChatInputApplicationCommandInteraction,
+    ApplicationCommandOptionType,
+    ApplicationCommandInteractionDataOptionString,
+    InteractionResponseType,
+    MessageFlags,
 } from 'discord-api-types'
 import {
-	interactionsLogger,
-	makeFumoResponseData,
-	fumoClient,
-	invite,
-	makePaginationResponseData,
-	buildRandomFumoComponents,
+    interactionsLogger,
+    makeFumoResponseData,
+    fumoClient,
+    invite,
+    makePaginationResponseData,
+    buildRandomFumoComponents,
 } from '../utils'
 import { Request, Response } from 'express'
 
 export async function handleCommands(
-	req: Request<never, never, APIChatInputApplicationCommandInteraction>,
-	res: Response<APIInteractionResponse>
+    req: Request<never, never, APIChatInputApplicationCommandInteraction>,
+    res: Response<APIInteractionResponse>
 ) {
-	const { data, member, guild_id } = req.body
+    const { data, member, guild_id } = req.body
 
-	interactionsLogger.info(
-		`user: ${member?.user?.username}#${member?.user?.discriminator} [${member?.user?.id}], command ${data.name}, guild id: ${guild_id}`
-	)
+    interactionsLogger.info(
+        `user: ${member?.user?.username}#${member?.user?.discriminator} [${member?.user?.id}], command ${data.name}, guild id: ${guild_id}`
+    )
 
-	switch (data.name) {
-		case 'get': {
-			const { value } = data.options?.find(
-				(option) => option.type === ApplicationCommandOptionType.String
-			) as ApplicationCommandInteractionDataOptionString
+    switch (data.name) {
+        case 'get': {
+            const { value } = data.options?.find(
+                (option) => option.type === ApplicationCommandOptionType.String
+            ) as ApplicationCommandInteractionDataOptionString
 
-			const fumo = fumoClient.cache.get(value) || fumoClient.cache.list[parseInt(value) - 1]
+            const fumo = fumoClient.cache.get(value) || fumoClient.cache.list[parseInt(value) - 1]
 
-			return res.json({
-				type: InteractionResponseType.ChannelMessageWithSource,
-				data: makeFumoResponseData(fumo),
-			})
-		}
+            return res.json({
+                type: InteractionResponseType.ChannelMessageWithSource,
+                data: makeFumoResponseData(fumo),
+            })
+        }
 
-		case 'random': {
-			const fumo = fumoClient.cache.random()
-			const randomFumoData = makeFumoResponseData(fumo)
+        case 'random': {
+            const fumo = fumoClient.cache.random()
+            const randomFumoData = makeFumoResponseData(fumo)
 
-			randomFumoData.components = [buildRandomFumoComponents(member?.user?.id!)]
+            randomFumoData.components = [buildRandomFumoComponents(member?.user?.id!)]
 
-			return res.json({
-				type: InteractionResponseType.ChannelMessageWithSource,
-				data: randomFumoData,
-			})
-		}
+            console.log(randomFumoData)
 
-		case 'list': {
-			const responseData = makePaginationResponseData(1, member?.user?.id!)
+            return res.json({
+                type: InteractionResponseType.ChannelMessageWithSource,
+                data: randomFumoData,
+            })
+        }
 
-			return res.json({
-				type: InteractionResponseType.ChannelMessageWithSource,
-				data: responseData,
-			})
-		}
+        case 'list': {
+            const responseData = makePaginationResponseData(1, member?.user?.id!)
 
-		case 'invite': {
-			return res.json({
-				type: InteractionResponseType.ChannelMessageWithSource,
-				data: {
-					content: `Invite me: [click here](${invite})`,
-					flags: MessageFlags.Ephemeral,
-				},
-			})
-		}
-	}
+            return res.json({
+                type: InteractionResponseType.ChannelMessageWithSource,
+                data: responseData,
+            })
+        }
+
+        case 'invite': {
+            return res.json({
+                type: InteractionResponseType.ChannelMessageWithSource,
+                data: {
+                    content: `Invite me: [click here](${invite})`,
+                    flags: MessageFlags.Ephemeral,
+                },
+            })
+        }
+    }
 }
