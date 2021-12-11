89eaeed16896bc7a6e7e8425fd4496938b27f5db
Leoxyzua <apinky549@gmail.com>
2021-12-10 21:49:03 -0500

    🐛 Fix port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config'
 import {
-	APIInteractionResponse,
-	APIInteraction,
-	InteractionType,
-	APIChatInputApplicationCommandInteraction,
-	APIMessageComponentInteraction,
+    APIInteractionResponse,
+    APIInteraction,
+    InteractionType,
+    APIChatInputApplicationCommandInteraction,
+    APIMessageComponentInteraction,
 } from 'discord-api-types/v9'
 
 import express, { Request } from 'express'
@@ -16,32 +16,32 @@ import { handleCommands } from './interactions/handleCommands'
 import { handleComponents } from './interactions/handleComponents'
 
 const app = express()
-	.use(bodyParser.json())
-	.get('*', (req, res) => res.send('this fumo is r'))
-	.post('/', (req: Request<never, APIInteractionResponse, APIInteraction>, res) => {
-		const signature = req.headers['x-signature-ed25519'] as string
-		const timestamp = req.headers['x-signature-timestamp'] as string
+    .use(bodyParser.json())
+    .get('*', (req, res) => res.send('this fumo is r'))
+    .post('/', (req: Request<never, APIInteractionResponse, APIInteraction>, res) => {
+        const signature = req.headers['x-signature-ed25519'] as string
+        const timestamp = req.headers['x-signature-timestamp'] as string
 
-		if (!verifyKey(JSON.stringify(req.body), signature, timestamp, process.env.PUBLIC_KEY!))
-			return res.status(401).end()
+        if (!verifyKey(JSON.stringify(req.body), signature, timestamp, process.env.PUBLIC_KEY!))
+            return res.status(401).end()
 
-		switch (req.body.type) {
-			case InteractionType.Ping:
-				interactionsLogger.info('got a pong interaction')
-				return res.json({ type: 1 })
+        switch (req.body.type) {
+            case InteractionType.Ping:
+                interactionsLogger.info('got a pong interaction')
+                return res.json({ type: 1 })
 
-			case InteractionType.ApplicationCommand:
-				return handleCommands(
-					req as Request<never, never, APIChatInputApplicationCommandInteraction>,
-					res
-				)
+            case InteractionType.ApplicationCommand:
+                return handleCommands(
+                    req as Request<never, never, APIChatInputApplicationCommandInteraction>,
+                    res
+                )
 
-			case InteractionType.MessageComponent:
-				return handleComponents(
-					req as Request<never, never, APIMessageComponentInteraction>,
-					res
-				)
-		}
-	})
+            case InteractionType.MessageComponent:
+                return handleComponents(
+                    req as Request<never, never, APIMessageComponentInteraction>,
+                    res
+                )
+        }
+    })
 
-app.listen(3000, () => logger.info('ready'))
+app.listen(process.env.PORT || 3000, () => logger.info('ready'))
